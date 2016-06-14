4499c08d89a166af17da05f6ff54b69ea777368d
Xiatao Jin <xiatao.jin@hulu.com>
2016-06-14 15:31:29 -0700

    Add all network support

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 
@@ -7,4 +9,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
